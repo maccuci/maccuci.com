526562a163c4d04f69e80a70cc746b0e8ad2d374
Maccuci <antoniogabrielleal@gmail.com>
2024-09-08 21:39:43 -0300

    edit index, blog page, add bookmark and garden section

---
+++ src/pages/bookmarks/items.ts
@@ -0,0 +1,13 @@
+type Props = {
+    name: string;
+    description: string;
+    href: string;
+}
+
+export const items: Props[] = [
+    {
+        name: "uiverse.io",
+        description: "Open-source styled components, like buttons, tooltip...",
+        href: "https://uiverse.io"
+    }
+]
---
+++ src/pages/garden/items.ts
@@ -0,0 +1,8 @@
+type Props = {
+    name: string;
+    href: string;
+}
+
+export const items: Props[] = [
+
+]
